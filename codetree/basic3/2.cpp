d98d03f1adee43e31106b3bb99c3f1d76746b8da
pasongsj <audrbekg@gmail.com>
2022-07-01 16:43:10 +0900

    0701_4

---
+++ codetree/basic3/2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int row, col;
+    cin >> row >> col;
+    cout << row + 8 << endl << col * 3 << endl << (row+8) * (col*3);
+    return 0;
+}
