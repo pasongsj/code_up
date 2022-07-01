8827163ececcd2f10c1a3fb0770573d0625e427e
pasongsj <audrbekg@gmail.com>
2022-07-01 15:08:06 +0900

    0701

---
+++ codetree/basic1/b_16.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int a = 1;
+    int b = 2;
+    string c = "C";
+    cout << a << "->" << b << "->" << c;
+    return 0;
+}
