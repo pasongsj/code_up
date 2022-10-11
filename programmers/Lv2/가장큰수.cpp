0776c14ddff31b477ed04005f7e8bad6af95ed7b
pasongsj <audrbekf@gamil.com>
2022-10-11 23:42:15 +0900

    fix

--- programmers/Lv2/가장큰수.cpp
+++ programmers/Lv2/가장큰수.cpp
@@ -12,7 +12,8 @@ string solution(vector<int> numbers) {
     string answer = "";
     sort(numbers.begin(),numbers.end(),cmp);
     for(int i=0;i<numbers.size();i++){
-        answer+=to_string(numbers[i]);
+        if(numbers[i] == 0 && answer == "0")   continue;
+	answer+=to_string(numbers[i]);
     }
     return answer;
 }
