fa5efe4bd2577ceb200938b9df1d89d6a772245f
SujeongLee <audrbekf@gmail.com>
2023-12-12 20:42:17 +0900

    231212 커밋

---
+++ baekjoon/1010/1010.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+int GetCase(int n, int r)
+{
+	int ans = 1;
+	for (int i = 0; i < r; ++i)
+	{
+		ans *= (n - i);
+		ans /= (i + 1);
+	}
+	return ans;
+}
+
+int main()
+{
+	int m;
+	std::cin >> m;
+	std::vector<int> answer;
+	
+	for (int i = 0; i < m; ++i)
+	{
+		int r, n;
+
+		std::cin >> r >> n;
+		answer.push_back(GetCase(n, r));
+	}
+	for (int ans : answer)
+	{
+		std::cout << ans << '\n';
+	}
+	return 0;
+}
