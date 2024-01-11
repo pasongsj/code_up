067e015c9ac3357a6d757081a6c24e3c4a623f41
SujeongLee <audrbekf@gmail.com>
2024-01-11 14:03:07 +0900

    240111커밋

--- baekjoon/2512/2512.cpp
+++ baekjoon/2512/2512.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 std::vector<int> regions;
-int n;
+int n,m;
 
 long long CalSum(int num)
 {
@@ -15,29 +15,23 @@ long long CalSum(int num)
 
 int main()
 {
-	int m;
 	// 0. 입력 받기
+	int max_num = 0;
 	std::cin >> n;
-	regions.resize(n);
-
-	int sum = 0, max = 0;
-	for (int i = 0; i < n; ++i)
-	{
-		std::cin >> regions[i];
-		sum += regions[i];
-		max = max > regions[i] ? max : regions[i];
+	for (auto i = 0; i < n; i++) {
+		int x;
+		std::cin >> x;
+		regions.push_back(x);
+		max_num = max_num > x ? max_num : x;
 	}
 	std::cin >> m;
 
+
 	// 1. 모든 요청이 배정될 수 있는 경우에는 요청한 금액을 그대로 배정한다.
-	if (sum < m)
-	{
-		return max;
-	}
 
 	// 2. 모든 요청이 배정될 수 없는 경우에는 특정한 정수 상한액을 계산하여 그 이상인 예산요청에는 모두 상한액을 배정한다. 
 	// 상한액 이하의 예산요청에 대해서는 요청한 금액을 그대로 배정한다. 
-	int left = 0, right = max;
+	int left = 0, right = max_num;
 	int answer = 0;
 	while (left <= right)
 	{
