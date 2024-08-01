c8cb89a202dc5d4f3d0f09cda8c37fb96b44496d
SujeongLee <audrbekf@gmail.com>
2024-08-02 08:14:04 +0900

    240730 백준 밀린커밋

---
+++ baekjoon/6550/6550.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+void Solution(const std::string& s, const std::string& t)
+{
+	int n_s = s.size();
+	int n_t = t.size();
+
+	int sindex = 0;
+	for (int i = 0; i < n_t; ++i)
+	{
+		if (t[i] == s[sindex])
+		{
+			sindex++;
+		}
+	}
+	if (sindex == n_s)
+	{
+		std::cout << "Yes\n";
+	}
+	else
+	{
+		std::cout << "No\n";
+	}
+}
+
+int main()
+{
+	std::string s, t; // ab abcd
+	while (std::cin >> s >> t)
+	{
+		Solution(s, t);
+	}
+	return 0;
+}
