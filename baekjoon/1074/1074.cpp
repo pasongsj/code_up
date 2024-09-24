94cd70bbbdeffae057d639559b8c299ae6c9783a
SujeongLee <audrbekf@gmail.com>
2024-09-24 22:50:56 +0900

    240924 백준 커밋

--- baekjoon/1074/1074.cpp
+++ baekjoon/1074/1074.cpp
@@ -5,18 +5,41 @@
 #include <vector>
 #include <cmath>
 
+
+int Getdiv(int n, int r, int c)
+{
+    if (n == 1)
+    {
+        return 0;
+    }
+    if (r < n / 2 && c < n / 2)
+    {
+        return Getdiv(n / 2, r, c);
+    }
+    else if (r < n / 2 && c >= n / 2)
+    {
+        return std::pow(n / 2, 2) + Getdiv(n / 2, r, c - n / 2);
+    }
+    else if (r >= n / 2 && c < n /2)
+    {
+        return std::pow(n / 2, 2) * 2 + Getdiv(n / 2, r - n / 2, c);
+    }
+    else
+    {
+		return std::pow(n / 2, 2) * 3 + Getdiv(n / 2, r - n / 2, c - n / 2);
+    }
+}
+
 int main()
 {
 	int n, r, c;
     std::cin >> n >> r >> c;
-    int N = std::pow(2, n);
-    std::vector<std::vector<int>> arr;
-    std::vector<std::vector<bool>> isvisited(n, std::vector<bool>(n, false));
-    std::vector<int> dir;
-    dir.resize(n);
-    int cnt = 0;
-    
 
+    int N = std::pow(2, n);
+    
+    int answer = Getdiv(N, r, c);
+
+    std::cout << answer;
 
     return 0;
 }
---
+++ baekjoon/15681/15681.cpp
@@ -0,0 +1,69 @@
+﻿// 15681.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+std::vector<bool> isvisited;
+std::vector<int> res;
+
+int DFS(int q, const std::vector<std::vector<int>>& links)
+{
+    int cnt = 1;
+    for (int i = 0; i < links[q].size(); ++i)
+    {
+        int next = links[q][i];
+        if (false == isvisited[next])
+        {
+            isvisited[next] = true;
+            cnt += DFS(next, links);
+        }
+    }
+
+    res[q] = cnt;
+    return cnt;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int N, R, Q;
+    std::cin >> N >> R >> Q;
+
+    isvisited.resize(N + 1);
+    res.resize(N + 1);
+
+    std::vector<std::vector<int>> links(N + 1, std::vector<int>());
+    for (int i = 0; i < N - 1; ++i)
+    {
+        int U, V;
+        std::cin >> U >> V;
+        links[U].push_back(V);
+        links[V].push_back(U);
+    }
+
+    isvisited[R] = true;
+    DFS(R, links);
+
+    for (int i = 0; i < Q; ++i)
+    {
+        int num;
+        std::cin >> num;
+        std::cout << res[num] << '\n';
+    }
+
+    return 0;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
